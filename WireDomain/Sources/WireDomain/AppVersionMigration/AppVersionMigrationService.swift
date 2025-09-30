30d39409bbfc72b0fbfafc210804fe50f69f2b19
Christoph Aldrian <christoph.aldrian@wire.com>
2025-09-30 13:50:34 +0200

    different approach

--- WireDomain/Sources/WireDomain/AppVersionMigration/AppVersionMigrationService.swift
+++ WireDomain/Sources/WireDomain/AppVersionMigration/AppVersionMigrationService.swift
@@ -115,7 +115,6 @@ public extension JournalProtocol {
 
     var lastCompletedAppVersionMigration: SemanticVersion? {
         get {
-            self[.lastCompletedAppVersionMigration] = "4.7.0" // TODO: delete
             return self[.lastCompletedAppVersionMigration].map(SemanticVersion.init)
         }
         set {
--- wire-ios-sync-engine/Source/UserSession/ZMUserSession/ZMUserSession.swift
+++ wire-ios-sync-engine/Source/UserSession/ZMUserSession/ZMUserSession.swift
@@ -1715,6 +1715,11 @@ extension ZMUserSession {
                 contextProvider: coreDataStack,
                 conversationLocalStore: clientSessionComponent?.conversationLocalStore,
                 protobufMessageProcessor: clientSessionComponent?.conversationProtobufMessageProcessor
+            ),
+            AppVersionMigration_4_9_0(
+                contextProvider: coreDataStack,
+                conversationLocalStore: clientSessionComponent?.conversationLocalStore,
+                protobufMessageProcessor: clientSessionComponent?.conversationProtobufMessageProcessor
             )
         ]
     }
